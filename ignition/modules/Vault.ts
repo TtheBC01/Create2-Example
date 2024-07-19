e9bd7d2f1dee0c5eb8472057ababb6ba520c6358
TtheBC01 <tac688@alumni.stanford.edu>
2024-07-18 23:56:28 -0700

    added create2 ignition module and configs

--- ignition/modules/Vault.ts
+++ ignition/modules/Vault.ts
@@ -1,11 +1,8 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import { parseEther } from "viem";
 
 const VaultModule = buildModule("VaultModule", (m) => {
 
-  const vault = m.contract("Vault", [], {
-    salt: "create2-example"
-  });
+  const vault = m.contract("Vault", [], {});
 
   return { vault };
 });
